18320ae57e1e654109e81ca05ee8f34e4a530439
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-07-12 18:37:15 -0500

    Updated script.js
    Replaced second part of vanilla code, "CrossOut", with jQuery code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,22 @@
 function newItem(){
 
     //javascript
-    //1. Adding a new item to the list of items: 
-       //let li = document.createElement("li");
-       //let inputValue = document.getElementById("input").value;
-       //let text = document.createTextNode(inputValue);
-       //li.appendChild(text);
-          
-       //if (inputValue === '') {
-       // alert("You must write something!");
-       //} else {
-       // let list = document.querySelector('#list');
-       // list.appendChild(li);
-       //}
-   
+    /*1. Adding a new item to the list of items:
+    
+    Vanilla Code:
+        let li = document.createElement("li");
+        let inputValue = document.getElementById("input").value;
+        let text = document.createTextNode(inputValue);
+        li.appendChild(text);
+    
+        if (inputValue === '') {
+          alert("You must write something!");
+        } else {
+          let list = document.querySelector('#list');
+          list.appendChild(li);
+        }
+        
+        Replaced vanilla code with jQuery code*/
 
        let li = $('<li></li>');
        inputValue = $('#input').val();
@@ -26,14 +29,34 @@ function newItem(){
           $('#list').append(li);
         }
     
-     //2. Crossing out an item from the list of items:
+        function crossOut() {
+          li.toggleClass("strike");
+      }
+  
+      li.on("dblclick", function crossOut() {
+          li.toggleClass("strike");
+      });
+
+     /*2. Crossing out an item from the list of items:
+
+     Vanilla Code:
        function crossOut() {
              li.classList.toggle("strike");
          }
     
          li.addEventListener("dblclick",crossOut);
+         
+        Replaced vanilla code with jQuery code*/
+
+        function crossOut() {
+          li.addClass("strike");
+      }
+  
+      li.on("dblclick", function crossOut() {
+          li.addClass("strike");
+      });
     
-     //3(i). Adding the delete button "X": 
+    /* //3(i). Adding the delete button "X": 
        let crossOutButton = document.createElement("crossOutButton");
          crossOutButton.appendChild(document.createTextNode("X"));
          li.appendChild(crossOutButton);
@@ -42,7 +65,7 @@ function newItem(){
      //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
        function deleteListItem(){
              li.classList.add("delete")
-         }
+         }*/
      // 4. Reordering the items: 
        $('#list').sortable();
     
